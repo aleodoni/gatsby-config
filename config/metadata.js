a78fb8d736393b8763408430296f2e7b0ea422a0
aleodoni <aleodoni@gmail.com>
2019-12-19 15:52:05 -0300

    gatsby initial config

---
+++ config/metadata.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: `Aleodoni`,
+  siteUrl: `http://www.aleodoni.com.br`,
+  description: `Site padrão para usar o Gatsby`,
+  social: [
+    {
+      name: 'Github',
+      url: 'https://github.com/aleodoni'
+    },
+    {
+      name: 'twitter',
+      url: 'https://twitter.com/aleodoni'
+    }
+  ],
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,51 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+ const siteMetadata = require('./config/metadata')
+
 module.exports = {
-  /* Your site config here */
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Aleodoni`,
+        short_name: `Aleodoni`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `tomato`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-9098878787",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    `gatsby-plugin-offline`
+  ]
 }
